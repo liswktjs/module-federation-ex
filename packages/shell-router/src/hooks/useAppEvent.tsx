14dde2095eb37d115b38437d8ec5f82f4e92a132
JASUN LEE <liswktjs@gmail.com>
2024-10-21 18:26:04 +0900

    feat: useAppEvent hook구현
    네비게이션 상태동기화 및 경로 변경 전파 및 수신 역할 수행

---
+++ packages/shell-router/src/hooks/useAppEvent.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+interface Props {
+  type: string;
+}
+
+const useAppEvent = ({ type }: Props) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const shellNavigationHandler = (e: Event) => {
+      const pathname = (e as CustomEvent<string>).detail;
+
+      if (location.pathname === pathname) {
+        return;
+      }
+
+      navigate(pathname);
+    };
+    window.addEventListener(`[app-shell] navigated`, shellNavigationHandler);
+  }, [type, navigate, location]);
+
+  useEffect(() => {
+    window.dispatchEvent(
+      new CustomEvent(`[${type}] navigated`, {
+        detail: location.pathname,
+      })
+    );
+  }, [location, type]);
+};
+
+export default useAppEvent;
