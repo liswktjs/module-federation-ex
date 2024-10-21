6bb4b30d33c42fc512c86490b3ee6937d515f913
JASUN LEE <liswktjs@gmail.com>
2024-10-21 14:54:44 +0900

    style:button style 추가

--- packages/ui-kit/src/components/Button.tsx
+++ packages/ui-kit/src/components/Button.tsx
@@ -1,10 +1,16 @@
 import { HTMLAttributes, PropsWithChildren } from "react";
+import styles from "./Button.module.css";
 
 const Button = ({
   children,
+  className,
   ...rest
 }: PropsWithChildren<HTMLAttributes<HTMLButtonElement>>) => {
-  return <button {...rest}>{children}</button>;
+  return (
+    <button className={`${styles.btn} ${className}`} {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
