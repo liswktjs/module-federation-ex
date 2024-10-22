9016819492c650954f629a5dc6f335749bf6ca48
JASUN LEE <liswktjs@gmail.com>
2024-10-22 14:46:36 +0900

    feat: shell-router function  export

---
+++ packages/shell-router/src/index.ts
@@ -0,0 +1,6 @@
+export { default as useAppEvent } from "./hooks/useAppEvent";
+export { default as useShellEvent } from "./hooks/useShellEvent";
+export { default as AppRoutingManager } from "./components/AppRoutingManager";
+export { injectFactory } from "./injector";
+
+export type * from "./types";
