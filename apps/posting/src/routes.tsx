0d861fb8142c66c9bc85e8e83822209dd075125e
JASUN LEE <liswktjs@gmail.com>
2024-10-24 16:22:26 +0900

    feat: posting에 Auth0의존성 주입 및 Home 연결

--- apps/posting/src/routes.tsx
+++ apps/posting/src/routes.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import { AppRoutingManager } from "@federation/shell-router";
 import { RouteObject } from "react-router-dom";
 import { POSTING_ELEMENT_ID } from "@federation/shared";
+import Auth0ClientProvider from "./provider/Auth0ClientProvider";
+import Home from "./pages/Home";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
-    element: <AppRoutingManager type={POSTING_ELEMENT_ID} />,
+    element: (
+      <Auth0ClientProvider>
+        <AppRoutingManager type={POSTING_ELEMENT_ID} />
+      </Auth0ClientProvider>
+    ),
     errorElement: <div>App Posting Error</div>,
-    children: [
-      { index: true, element: <div>App Posting Root</div> },
-      {
-        path: "1",
-        element: <div>App Posting Page 1</div>,
-      },
-    ],
+    children: [{ index: true, element: <Home /> }],
   },
 ];
