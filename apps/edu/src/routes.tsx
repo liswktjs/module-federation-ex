cf926751a46adc2b8f6cb80efe47769c684e5741
JASUN LEE <liswktjs@gmail.com>
2024-10-25 13:58:50 +0900

    feat: edu router에 layout과 course list, course detail page연동

--- apps/edu/src/routes.tsx
+++ apps/edu/src/routes.tsx
@@ -2,17 +2,27 @@ import React from "react";
 import { AppRoutingManager } from "@federation/shell-router";
 import { RouteObject } from "react-router-dom";
 import { Auth0ClientProvider, EDU_ELEMENT_ID } from "@federation/shared";
-import Home from "./pages/Home";
+import Layout from "./components/Layout";
+import CourseList from "./pages/CourseList";
+import CourseDetail from "./pages/CourseDetail";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: (
       <Auth0ClientProvider>
-        <AppRoutingManager type={EDU_ELEMENT_ID} />
+        <Layout>
+          <AppRoutingManager type={EDU_ELEMENT_ID} />
+        </Layout>
       </Auth0ClientProvider>
     ),
     errorElement: <div>App Edu Error</div>,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <CourseList /> },
+      {
+        path: ":id",
+        element: <CourseDetail />,
+      },
+    ],
   },
 ];
