d81ad3919e8a1dd0e56a81efd3a1f355aca95659
JASUN LEE <liswktjs@gmail.com>
2024-10-27 23:12:46 +0900

    feat: 이미지 없을 때 대표 이미지 컴포넌트 구현

---
+++ apps/network/src/assets/IconDefault.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const IconDefault: React.FC = () => (
+  <img
+    className="w-20 h-20 object-contain"
+    src="https://linefriendssquare.com/cdn/shop/files/line-friends-keyring---40421958975687.jpg?v=1726804926&width=1346"
+  />
+);
+
+export default IconDefault;
