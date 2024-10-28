2058488a13829bd2834761d10ba107da63b6b302
JASUN LEE <liswktjs@gmail.com>
2024-10-28 14:30:23 +0900

    feat: 이미지 없을 때 보여줄 default 컴포넌트 구현

---
+++ apps/job/src/assets/IconDefault.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const IconDefault: React.FC = () => (
+  <img
+    src="https://linefriendssquare.com/cdn/shop/files/line-friends-keyring---40421958975687.jpg?v=1726804926&width=1346"
+    style={{ width: "40px", height: "40px", objectFit: "contain" }}
+  />
+);
+
+export default IconDefault;
