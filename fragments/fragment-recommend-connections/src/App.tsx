81f2221861956c8dd49d67fc478548e577dfb7b8
JASUN LEE <liswktjs@gmail.com>
2024-11-15 15:33:16 +0900

    chore:  fragments 저장소 생성

---
+++ fragments/fragment-recommend-connections/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <div className="container">
+    <div>Name: fragment-recommend-connections</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
